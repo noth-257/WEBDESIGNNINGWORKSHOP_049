49dcc7f8cfcba6eec60228e1783c009112c95950
Abhishek Pratap Singh <pratapabhishek257@gmail.com>
2026-03-19 09:38:47 +0530

    js basics:

---
+++ javascript/javascript.fundamentals/app.js
@@ -0,0 +1,48 @@
+// var name="Abhishek Pratap Singh";
+// var age=30;
+// var isemployed=true;
+// console.log(name);
+// console.log(age);
+// console.log(isemployed);
+// var age=50;
+// console.log(age);
+
+// let name="Abhishek Pratap Singh";
+// let age=50;
+// console.log(name);
+// console.log(age);
+// let age=30;
+// console.log(age);
+
+// if(false){
+//     var age=30;
+// }
+//     console.log(age);
+
+// if(true){
+//     let age=30;
+// }
+// console.log(age);
+
+
+// const PI=3.14;
+// console.log(PI);
+
+// PI=3.14159;
+// console.log(PI);
+
+// console.log(typeof name)
+// console.log(typeof age)
+// console.log(typeof isemployed)
+
+// let name="Abhishek pratap Singh";
+// let age=10;
+// console.log("my name is " + name + " and my age is " + age);
+// console.log(`my name is ${name} and my age is ${age}`);
+
+//boolean
+let ispass=true;
+let isabsent=false;
+console.log(ispass);
+console.log(isabsent);
+console.log(typeof ispass);
